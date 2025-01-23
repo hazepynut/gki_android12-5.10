25fcee7c735c657d9e85827ef0b8d1c9d88c6675
Asteroidd21 <138986969+Asteroidd21@users.noreply.github.com>
2025-01-24 06:27:33 +0800

    kernel/irq: chip.c: fix all issues in irq_startup()

--- kernel/irq/chip.c
+++ kernel/irq/chip.c
@@ -255,40 +255,46 @@ static int __irq_startup(struct irq_desc *desc)
 
 int irq_startup(struct irq_desc *desc, bool resend, bool force)
 {
-	struct irq_data *d = irq_desc_get_irq_data(desc);
-	struct cpumask *aff = irq_data_get_affinity_mask(d);
-	int ret = 0;
+    struct irq_data *d = irq_desc_get_irq_data(desc);
+    struct cpumask *aff = irq_data_get_affinity_mask(d);
+    int ret = 0;
 
-	desc->depth = 0;
+    desc->depth = 0;
 
-	if (irqd_is_started(d)) {
-		irq_enable(desc);
-	} else {
-		switch (__irq_startup_managed(desc, aff, force)) {
-		case IRQ_STARTUP_NORMAL:
-			if (d->chip->flags & IRQCHIP_AFFINITY_PRE_STARTUP)
-				irq_setup_affinity(desc);
-			ret = __irq_startup(desc);
-			if (!(d->chip->flags & IRQCHIP_AFFINITY_PRE_STARTUP))
-				if (irqd_has_set(&desc->irq_data, IRQD_PERF_CRITICAL)) {
-					setup_perf_irq_locked(desc, desc->action->flags);
-				} else {
-					irq_setup_affinity(desc);
-				}
-			break;
-		case IRQ_STARTUP_MANAGED:
-			irq_do_set_affinity(d, aff, false);
-			ret = __irq_startup(desc);
-			break;
-		case IRQ_STARTUP_ABORT:
-			irqd_set_managed_shutdown(d);
-			return 0;
-		}
-	}
-	if (resend)
-		check_irq_resend(desc, false);
+    if (irqd_is_started(d)) {
+        irq_enable(desc);
+    } else {
+        switch (__irq_startup_managed(desc, aff, force)) {
+        case IRQ_STARTUP_NORMAL: {
+            if (d->chip->flags & IRQCHIP_AFFINITY_PRE_STARTUP) {
+                irq_setup_affinity(desc);
+            }
+            ret = __irq_startup(desc);
+            if (!(d->chip->flags & IRQCHIP_AFFINITY_PRE_STARTUP)) {
+                if (irqd_has_set(&desc->irq_data, IRQD_PERF_CRITICAL)) {
+                    setup_perf_irq_locked(desc, desc->action->flags);
+                } else {
+                    irq_setup_affinity(desc);
+                }
+            }
+            break;
+        }
+        case IRQ_STARTUP_MANAGED: {
+            irq_do_set_affinity(d, aff, false);
+            ret = __irq_startup(desc);
+            break;
+        }
+        case IRQ_STARTUP_ABORT: {
+            irqd_set_managed_shutdown(d);
+            return 0;
+        }
+        }
+    }
+    if (resend) {
+        check_irq_resend(desc, false);
+    }
 
-	return ret;
+    return ret;
 }
 
 int irq_activate(struct irq_desc *desc)
